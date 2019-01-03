18b6ea7d24b0bb31a11088cf7609a90a4a897e89
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2019-01-02 22:00:23 -0600

    Updates to automatically install dependents

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/josephjcontreras/FEMethods.git',
     packages=find_packages(),
+    install_requires=['numpy', 'matplotlib', 'scipy'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Development Status::3 - Alpha'
+        'Operating System :: OS Independent'
     ],
 )
