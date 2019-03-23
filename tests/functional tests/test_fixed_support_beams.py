54a856973b164586d4e7487059adf75f7980cf84
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2019-03-23 10:37:31 -0500

    Add functional tests for fixed beam

---
+++ tests/functional tests/test_fixed_support_beams.py
@@ -0,0 +1,38 @@
+"""
+Functional tests for beams with fixed supports
+
+https://www.awc.org/pdf/codes-standards/publications/design-aids/AWC-DA6-BeamFormulas-0710.pdf
+
+"""
+
+import pytest
+from settings import E, EI, Ixx, L, P, TOL
+from validate import validate
+
+from femethods.elements import Beam
+from femethods.loads import PointLoad
+from femethods.reactions import FixedReaction
+
+
+def test_cantilevered_beam_load_at_end():
+    """fixed beam with concentrated load at free end
+    case 13
+    """
+
+    R = -P
+    M_max = P * L  # at fixed end
+
+    d_max = P * L ** 3 / (3 * EI)  # at free end
+
+    beam = Beam(
+        length=L,
+        loads=[PointLoad(value=P, location=0)],
+        reactions=[FixedReaction(L)],
+        E=E,
+        Ixx=Ixx,
+    )
+    beam.solve()
+
+    validate(beam, loc=0, R=[(R, M_max)], M_loc=0, d_loc=d_max)
+
+    assert pytest.approx(beam.moment(L), rel=TOL) == M_max
