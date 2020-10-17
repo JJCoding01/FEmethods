64d591fc7c52d3a0947a137a0aa1617efa89c39b
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2020-10-17 16:44:27 -0500

    Update version and copyright date

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='femethods',
-    version='0.1.7a1',
+    version='0.1.7a2',
     author='Joseph Contreras',
     author_email='26684136+JosephJContreras@users.noreply.github.com',
     description='Implementation of Finite Element Analysis',
