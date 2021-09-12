74415c1d309f334057fa46d1a7613ad831666026
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2021-09-12 12:42:31 -0500

    Remove mypy typing
    I'm not ready to implement this throughout the project and have these
    properly configured.

--- femethods/core/forces.py
+++ femethods/core/forces.py
@@ -2,50 +2,46 @@
 Base module that contains base classes to be used by other modules
 """
 
-from typing import Optional
-
 
 class Forces:
     """Base class for all loads and reactions"""
 
-    def __init__(
-        self, magnitude: Optional[float], location: float = 0
-    ) -> None:
+    def __init__(self, magnitude, location=0):
         self.magnitude = magnitude
         self.location = location
 
     @property
-    def magnitude(self) -> Optional[float]:
+    def magnitude(self):
         return self._magnitude
 
     @magnitude.setter
-    def magnitude(self, magnitude: float) -> None:
+    def magnitude(self, magnitude):
         if not isinstance(magnitude, (int, float, type(None))):
             raise TypeError("force value must be a number")
         self._magnitude = magnitude
 
     @property
-    def location(self) -> float:
+    def location(self):
         return self._location
 
     @location.setter
-    def location(self, location: float) -> None:
+    def location(self, location):
         if location < 0:
             # location must be positive to be a valid length/position
             raise ValueError("location must be positive!")
         self._location = location
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return (
             f"{self.__class__.__name__}(magnitude={self.magnitude}, "
             + f"location={self.location})"
         )
 
-    def __add__(self, force2: "Forces") -> Optional["Forces"]:
+    def __add__(self, force2):
 
-        # assert to validate type checking for mypy
-        assert self.magnitude is not None
-        assert force2.magnitude is not None
+        if not isinstance(force2, self.__class__):
+            # only addition between forces is implemented
+            raise TypeError(f"cannot add {self.__class__} and {type(force2)}")
 
         f1 = self.magnitude
         x1 = self.location
@@ -67,10 +63,7 @@ class Forces:
             self.magnitude * self.location == other.magnitude * other.location
         )
 
-    def __sub__(self, force2: "Forces") -> Optional["Forces"]:
-
-        assert self.magnitude is not None
-        assert force2.magnitude is not None
+    def __sub__(self, force2):
 
         f1 = self.magnitude
         x1 = self.location
