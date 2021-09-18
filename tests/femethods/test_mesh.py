79329850ec5be4654f45423ae47a9ea23eab1b34
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2021-09-18 12:28:32 -0500

    Reformat test_mesh.py into separate tests

--- tests/femethods/test_mesh.py
+++ tests/femethods/test_mesh.py
@@ -14,21 +14,35 @@ def mesh():
     return Mesh(LENGTH, LOADS, REACTIONS, dof=2)
 
 
-def test_mesh_properties(mesh):
-    # there should be a node at the start and end of the beam, as well as a
-    # node for any loads
-    assert mesh.nodes == [0, 15, 25], "Mesh nodes do not match expected"
-    assert mesh.dof == 6, "Wrong number of global degrees-of-freedom"
-    assert mesh.lengths == [
-        15,
-        10,
-    ], "Mesh elements do not have expected lengths"
+def test_mesh_nodes(mesh):
+    expected_nodes = [0, 15, 25]
+    assert expected_nodes == mesh.nodes
+
+
+def test_mesh_dof(mesh):
+    assert mesh.dof == 6
+
+
+def test_mesh_lengths(mesh):
+    expected_lengths = [15, 10]
+    assert expected_lengths == mesh.lengths
+
+
+def test_element_count(mesh):
     assert mesh.num_elements == 2, "Mesh element count does not match"
     assert mesh.num_elements == len(mesh.lengths)
 
+
+def test_nodes_read_only(mesh):
     with pytest.raises(AttributeError):
         mesh.nodes = "Mesh nodes property is read-only"
+
+
+def test_dof_read_only(mesh):
     with pytest.raises(AttributeError):
         mesh.dof = "Mesh dof should be read-only"
+
+
+def test_lengths_read_only(mesh):
     with pytest.raises(AttributeError):
         mesh.lengths = "Mesh element lengths are read-only"
