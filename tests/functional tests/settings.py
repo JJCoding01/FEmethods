fb503eccf0e791f4f2760d97563a4bbc2bb0b886
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2021-09-25 22:41:47 -0500

    Remove length and load parameters; these have been parametrized

--- tests/functional tests/settings.py
+++ tests/functional tests/settings.py
@@ -3,8 +3,6 @@ Common constants and parameters to be used functional tests. This is done so tha
 the beams are all similar in size and loading.
 """
 
-L = 120  # in, length of beam in inches
-P = -1000  # lbs, load acting on beam
 E = 29e6  # psi, Young's modulus
 Ixx = 350  # in^4 area moment of inertia of beam
 EI = E * Ixx  # common constant
