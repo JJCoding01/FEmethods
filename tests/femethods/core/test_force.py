fd05b26ba023aa1bbcfb4badcd3d1ad6edae4bae
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2021-12-28 22:50:51 -0600

    Update force tests to use ForceFactory

--- tests/femethods/core/test_force.py
+++ tests/femethods/core/test_force.py
@@ -3,35 +3,41 @@
 
 import pytest
 
-from femethods.core import Force
+from tests.factories import ForceFactory
 
 
-def test_init_force():
-    force = Force(magnitude=500, location=50)
-
+def test_init_force_magnitude():
+    force = ForceFactory(magnitude=500)
     assert force.magnitude == 500
-    assert force.location == 50
 
 
-def test_force_update():
-    p = Force(magnitude=-5, location=25)
+def test_init_force_location():
+    force = ForceFactory(location=10)
+    assert force.location == 10
 
-    p.magnitude = -100
-    p.location = 30
-    assert p.magnitude == -100
-    assert p.location == 30
+
+def test_force_update_magnitude():
+    force = ForceFactory(magnitude=-5)
+    force.magnitude = -100
+    assert force.magnitude == -100
+
+
+def test_force_update_location():
+    force = ForceFactory(location=15)
+    force.location = 25
+    assert force.location == 25
 
 
 @pytest.mark.parametrize(
     "force",
     [
-        Force(250, 15),
-        Force(250 * 15 / 10, 10),
-        Force(250 * 15 / 25, 25),
+        ForceFactory(magnitude=250, location=15),
+        ForceFactory(magnitude=250 * 15 / 10, location=10),
+        ForceFactory(magnitude=250 * 15 / 25, location=25),
     ],
 )
 def test_force_eq(force):
-    base_force = Force(250, 15)
+    base_force = ForceFactory(magnitude=250, location=15)
 
     assert base_force == force
 
@@ -39,11 +45,23 @@ def test_force_eq(force):
 @pytest.mark.parametrize(
     "force1, force2",
     [
-        (Force(250, 15), Force(250, 0)),
-        (Force(250, 15), Force(250, 10)),
-        (Force(250, 15), Force(None, 0)),
-        (Force(None, 15), Force(None, 0)),
-        (Force(250, 15), "not a force"),
+        (
+            ForceFactory(magnitude=250, location=15),
+            ForceFactory(magnitude=250, location=0),
+        ),
+        (
+            ForceFactory(magnitude=250, location=15),
+            ForceFactory(magnitude=250, location=10),
+        ),
+        (
+            ForceFactory(magnitude=250, location=15),
+            ForceFactory(magnitude=None, location=0),
+        ),
+        (
+            ForceFactory(magnitude=None, location=15),
+            ForceFactory(magnitude=None, location=0),
+        ),
+        (ForceFactory(magnitude=250, location=15), "not a force"),
     ],
 )
 def test_force_ne(force1, force2):
@@ -52,8 +70,8 @@ def test_force_ne(force1, force2):
 
 @pytest.mark.parametrize("force, location", [(500, 10), (250, 50)])
 def test_force_add(force, location):
-    force1 = Force(250, 25)
-    force2 = Force(force, location)
+    force1 = ForceFactory(magnitude=250, location=25)
+    force2 = ForceFactory(magnitude=force, location=location)
 
     force_net = force1 + force2
 
@@ -65,27 +83,27 @@ def test_force_add(force, location):
 
     x = (f1 * x1 + f2 * x2) / (f1 + f2)
 
-    force_expected = Force(f1 + f2, x)
+    force_expected = ForceFactory(magnitude=f1 + f2, location=x)
 
     assert force_expected == force_net
 
 
 def test_force_add_pair():
-    p1 = Force(magnitude=-2, location=10)
-    p2 = Force(magnitude=-8, location=20)
-    assert p1 + p2 == Force(magnitude=-10, location=18)
+    p1 = ForceFactory(magnitude=-2, location=10)
+    p2 = ForceFactory(magnitude=-8, location=20)
+    assert p1 + p2 == ForceFactory(magnitude=-10, location=18)
 
 
 def test_force_add_invalid():
     with pytest.raises(TypeError):
         # noinspection PyTypeChecker
-        Force(250, 25) + 50
+        ForceFactory(magnitude=250, location=25) + 50
 
 
 @pytest.mark.parametrize("force, location", [(500, 10), (100, 50)])
 def test_force_sub(force, location):
-    force1 = Force(250, 25)
-    force2 = Force(force, location)
+    force1 = ForceFactory(magnitude=250, location=25)
+    force2 = ForceFactory(magnitude=force, location=location)
 
     force_net = force1 - force2
 
@@ -97,14 +115,14 @@ def test_force_sub(force, location):
 
     x = (f1 * x1 - f2 * x2) / (f1 - f2)
 
-    force_expected = Force(f1 - f2, x)
+    force_expected = ForceFactory(magnitude=f1 - f2, location=x)
 
     assert force_expected == force_net
 
 
 def test_force_arithmetic():
-    p1 = Force(magnitude=10, location=10)
-    p2 = Force(magnitude=5, location=20)
-    p3 = Force(magnitude=5, location=0)
+    p1 = ForceFactory(magnitude=10, location=10)
+    p2 = ForceFactory(magnitude=5, location=20)
+    p3 = ForceFactory(magnitude=5, location=0)
     assert p1 - p2 == p3
     assert p3 + p2 == p1
