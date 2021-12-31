4851c5a9b6cda2c653c043f88d528fe6d8044e67
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2021-12-31 15:20:26 -0600

    Update Reaction tests to use ReactionFactory and boundary parameter

--- tests/femethods/test_reactions.py
+++ tests/femethods/test_reactions.py
@@ -1,75 +1,142 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
 import pytest
 
-from femethods.reactions import FixedReaction, PinnedReaction, Reaction
+from tests.factories import ReactionFactory
 
 
-@pytest.mark.parametrize(
-    "reaction, name", ((PinnedReaction, "pinned"), (FixedReaction, "fixed"))
-)
-def test_reaction_name(reaction, name):
-    r = reaction(0)
-    assert r.name == name
+@pytest.mark.parametrize("location", (0, 5, 10))
+def test_reaction_location_valid_init(location):
+    reaction = ReactionFactory(location=location)
+    assert reaction.location == location
 
 
-@pytest.mark.parametrize("reaction", (PinnedReaction, FixedReaction))
-def test_reaction_init(reaction):
-    r = reaction(0)
-    assert r.force is None
-    assert r.moment is None
-
-
-@pytest.mark.parametrize("invalid_type", (None, "str", (0, 0)))
-def test_invalid_location_type(invalid_type):
-    with pytest.raises(TypeError):
-        Reaction(location=invalid_type)
-
-
-def test_negative_location():
+def test_reaction_location_invalid_negative():
     with pytest.raises(ValueError):
-        Reaction(location=-5)
+        ReactionFactory(location=-5)
 
 
-@pytest.mark.parametrize("moment", (None, -100, 0, 100))
-@pytest.mark.parametrize("force", (None, -100, 0, 100))
-@pytest.mark.parametrize("location", (0, 10))
-def test_reaction_value(location, force, moment):
-    r = Reaction(location)
-    r.force = force
-    r.moment = moment
-    assert r.value == (force, moment)
+@pytest.mark.parametrize("invalid_location", (None, "str", [0]))
+def test_reaction_location_invalid(invalid_location):
+    with pytest.raises(TypeError):
+        ReactionFactory(location=invalid_location)
 
 
-def test_reaction_value_invalidate():
-    r = Reaction(0)
+@pytest.mark.parametrize("bc1", (None, 0))
+def test_reaction_boundary_valid_len1(bc1):
+    if bc1 is None:
+        pytest.skip("skip None boundary")
+    reaction = ReactionFactory(boundary=bc1)
+    assert reaction.boundary == bc1
+
+
+@pytest.mark.parametrize("bc1", (None, 0))
+@pytest.mark.parametrize("bc2", (None, 0))
+def test_reaction_boundary_valid_len2(bc1, bc2):
+    boundary = (bc1, bc2)
+    if boundary == (None, None):
+        pytest.skip("skip (None, None) boundary")
+    reaction = ReactionFactory(boundary=boundary)
+    assert reaction.boundary == boundary
+
+
+@pytest.mark.parametrize("bc1", (None, 0))
+@pytest.mark.parametrize("bc2", (None, 0))
+@pytest.mark.parametrize("bc3", (None, 0))
+def test_reaction_boundary_valid_len3(bc1, bc2, bc3):
+    boundary = (bc1, bc2, bc3)
+    if boundary == (None, None, None):
+        pytest.skip("skip (None, None, None) boundary")
+    reaction = ReactionFactory(boundary=boundary)
+    assert reaction.boundary == boundary
+
+
+@pytest.mark.parametrize("bc1", ("str", 1, 2))
+def test_reaction_boundary_invalid_len1(bc1):
+    with pytest.raises(ValueError):
+        ReactionFactory(boundary=bc1)
+
+
+@pytest.mark.parametrize("bc1", (None, "str", 1, 2))
+@pytest.mark.parametrize("bc2", (None, "str", 1, 2))
+def test_reaction_boundary_invalid_len2(bc1, bc2):
+    boundary = (bc1, bc2)
+    if boundary == (None, None):
+        pytest.skip("skip (None, None) boundary")
+    with pytest.raises(ValueError):
+        ReactionFactory(boundary=boundary)
+
+
+@pytest.mark.parametrize("bc1", (None, "str", 1, 2))
+@pytest.mark.parametrize("bc2", (None, "str", 1, 2))
+@pytest.mark.parametrize("bc3", (None, "str", 1, 2))
+def test_reaction_boundary_invalid_len3(bc1, bc2, bc3):
+    boundary = (bc1, bc2, bc3)
+    if boundary == (None, None, None):
+        pytest.skip("skip (None, None, None) boundary")
+    with pytest.raises(ValueError):
+        ReactionFactory(boundary=boundary)
+
+
+@pytest.mark.parametrize("boundary", (None, (None, None), (None, None, None)))
+def test_reaction_boundary_useless_none(boundary):
+    with pytest.warns(UserWarning):
+        ReactionFactory(boundary=boundary)
+
+
+@pytest.mark.parametrize("force", (-100, 0, 100))
+def test_reaction_force(force):
+    reaction = ReactionFactory()
+    reaction.force = force
+    assert reaction.force == force
+
+
+@pytest.mark.parametrize("moment", (-150, 0, 150))
+def test_reaction_moment(moment):
+    reaction = ReactionFactory()
+    reaction.moment = moment
+    assert reaction.moment == moment
+
+
+@pytest.mark.parametrize("force", (-200, 0, 200))
+@pytest.mark.parametrize("moment", (-150, 0, 150))
+def test_reaction_value(force, moment):
+    reaction = ReactionFactory()
+    reaction.force = force
+    reaction.moment = moment
+    assert reaction.value == (force, moment)
+
+
+@pytest.mark.parametrize("location", (0, 15, 20))
+def test_reaction_invalidate_on_location_update(location):
+    reaction = ReactionFactory()
+    reaction.force = 100
+    reaction.moment = 150
+    assert reaction.value == (100, 150)
+
+    # update location
+    reaction.location = location
+
+    # assert the force/moment was invalidated
+    assert reaction.value == (None, None)
+
+
+def test_reaction_invalidate_manual():
+    r = ReactionFactory()
     r.force = 100
     r.moment = 15
+    assert r.value == (100, 15)
     r.invalidate()
     assert r.value == (None, None), "reaction not invalidated"
-
-
-def test_reaction_invalidated_when_moved():
-    r = Reaction(0)
-    r.force = 100
-    r.moment = 200
-
-    r.location = 10  # move reaction
-    assert r.value == (None, None), "reaction not invalided"
-
-
-@pytest.mark.parametrize(
-    "reaction, bc", ((PinnedReaction, (0, None)), (FixedReaction, (0, 0)))
-)
-def test_reaction_boundary_conditions(reaction, bc):
-    r = reaction(0)
-    assert r.boundary == bc, "boundary conditions does not match expected value"
 
 
 @pytest.mark.parametrize("location", (0, 10))
 @pytest.mark.parametrize("force", (None, -10, 0, 10))
 @pytest.mark.parametrize("moment", (None, -10, 0, 10))
 def test_reaction_eq(location, force, moment):
-    r1 = Reaction(location)
-    r2 = Reaction(location)
+    r1 = ReactionFactory(location=location)
+    r2 = ReactionFactory(location=location)
 
     r1.force = force
     r1.moment = moment
@@ -81,14 +148,14 @@ def test_reaction_eq(location, force, moment):
 
 @pytest.mark.parametrize("other", (None, 10, "str"))
 def test_reaction_not_eq_type(other):
-    assert Reaction(0) != other
+    assert ReactionFactory() != other
 
 
 @pytest.mark.parametrize("force", (None, -100, 0, 100))
 @pytest.mark.parametrize("moment", (None, -100, 0, 100))
 def test_reaction_not_eq_location(force, moment):
-    r1 = Reaction(0)
-    r2 = Reaction(10)
+    r1 = ReactionFactory(location=0)
+    r2 = ReactionFactory(location=10)
 
     r1.force = force
     r1.moment = moment
@@ -101,8 +168,8 @@ def test_reaction_not_eq_location(force, moment):
 @pytest.mark.parametrize("location", (0, 100))
 @pytest.mark.parametrize("moment", (None, -100, 0, 100))
 def test_reaction_not_eq_force(location, moment):
-    r1 = Reaction(location)
-    r2 = Reaction(location)
+    r1 = ReactionFactory(location=location)
+    r2 = ReactionFactory(location=location)
 
     r1.force = 0
     r2.force = 10
@@ -116,8 +183,8 @@ def test_reaction_not_eq_force(location, moment):
 @pytest.mark.parametrize("location", (0, 100))
 @pytest.mark.parametrize("force", (None, -100, 0, 100))
 def test_reaction_not_eq_moment(location, force):
-    r1 = Reaction(location)
-    r2 = Reaction(location)
+    r1 = ReactionFactory(location=location)
+    r2 = ReactionFactory(location=location)
 
     r1.force = force
     r2.force = force
