1fa4e8e75b98e84cb3bcc82659ce7089bfbfff51
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2021-12-31 14:56:39 -0600

    Add ReactionFactory

--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -2,3 +2,4 @@
 # flake8: noqa
 
 from .core import ForceFactory
+from .reactions import ReactionFactory
---
+++ tests/factories/reactions/__init__.py
@@ -0,0 +1,4 @@
+# pylint: disable=F
+# flake8: noqa
+
+from .reaction import ReactionFactory
