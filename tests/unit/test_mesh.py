2b017aa6220e1cde28b3e065b227675770f79446
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-01-01 15:35:46 -0600

    Update Mesh tests after Mesh refactor; use MastFactory

--- tests/unit/test_mesh.py
+++ tests/unit/test_mesh.py
@@ -1,63 +1,77 @@
 # pylint: disable=missing-function-docstring
 # pylint: disable=redefined-outer-name
-
+import numpy as np
 import pytest
 
-from femethods.loads import PointLoad
-from femethods.mesh import Mesh
-from femethods.reactions import FixedReaction
-
-LOADS = [PointLoad(magnitude=-1000, location=15)]
-REACTIONS = [FixedReaction(0)]
-LENGTH = 25
-
-
-@pytest.fixture
-def mesh():
-    return Mesh(LENGTH, LOADS, REACTIONS, node_dof=2)
+from tests.factories import MeshFactory
 
 
 @pytest.mark.parametrize("dof", (-5, -4, 0, 3.25))
 def test_mesh_node_dof_invalid_number(dof):
     with pytest.raises(ValueError):
-        Mesh(LENGTH, LOADS, REACTIONS, node_dof=dof)
+        MeshFactory(node_dof=dof)
+
+
+@pytest.mark.parametrize("dof", (None, "1", []))
+def test_mesh_node_dof_invalid_type(dof):
+    with pytest.raises(TypeError):
+        MeshFactory(node_dof=dof)
 
 
 @pytest.mark.parametrize("dof", (2, 4, 5))
 def test_mesh_node_dof_valid_number(dof):
-    mesh = Mesh(LENGTH, LOADS, REACTIONS, node_dof=dof)
+    mesh = MeshFactory(node_dof=dof)
     assert mesh.node_dof == dof
 
 
-def test_mesh_nodes(mesh):
-    expected_nodes = [0, 15, 25]
+@pytest.mark.parametrize("length", (20, 25))
+@pytest.mark.parametrize("locations", ([1, 5, 7], [15, 12, 5], [3, 7, 10, 15]))
+def test_mesh_nodes(length, locations):
+    expected_nodes = set(locations)
+    expected_nodes.add(0)
+    expected_nodes.add(length)
+    expected_nodes = sorted(expected_nodes)
+
+    mesh = MeshFactory(length=length, locations=locations)
     assert expected_nodes == mesh.nodes
 
 
-def test_mesh_dof(mesh):
-    assert mesh.dof == 6
+@pytest.mark.parametrize("node_dof", (1, 2, 3))
+@pytest.mark.parametrize("locations", ([3], [3, 10], [5, 10, 15], [5, 10, 15, 20]))
+def test_mesh_dof(node_dof, locations):
+    mesh = MeshFactory(length=25, locations=locations, node_dof=node_dof)
+
+    # note, add 2 to account for ends
+    expected_dof = (len(locations) + 2) * node_dof
+    assert mesh.dof == expected_dof
 
 
-def test_mesh_lengths(mesh):
-    expected_lengths = [15, 10]
-    assert expected_lengths == mesh.lengths
+@pytest.mark.parametrize("total_length", [25, 35])
+@pytest.mark.parametrize("locations", ([5, 10], [5, 10, 15], [5, 10, 15, 20]))
+def test_mesh_lengths(total_length, locations):
+    mesh = MeshFactory(length=total_length, locations=locations)
+
+    locations.append(0)
+    locations.append(total_length)
+    locations = sorted(set(locations))
+    expected_lengths = np.diff(locations)
+
+    assert np.allclose(expected_lengths, mesh.lengths)
 
 
-def test_element_count(mesh):
-    assert mesh.num_elements == 2, "Mesh element count does not match"
-    assert mesh.num_elements == len(mesh.lengths)
-
-
-def test_nodes_read_only(mesh):
+def test_nodes_read_only():
+    mesh = MeshFactory()
     with pytest.raises(AttributeError):
         mesh.nodes = "Mesh nodes property is read-only"
 
 
-def test_dof_read_only(mesh):
+def test_dof_read_only():
+    mesh = MeshFactory()
     with pytest.raises(AttributeError):
         mesh.dof = "Mesh dof should be read-only"
 
 
-def test_lengths_read_only(mesh):
+def test_lengths_read_only():
+    mesh = MeshFactory()
     with pytest.raises(AttributeError):
         mesh.lengths = "Mesh element lengths are read-only"
