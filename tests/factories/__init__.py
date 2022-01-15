bb36e38df6d6187897ec2b955fbe92b36fad6cbd
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-01-14 21:00:42 -0600

    Add factory for distributed load

--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -3,6 +3,11 @@
 
 from .core import ForceFactory
 from .elements import PropertyFactory
-from .loads import LoadFactory, MomentLoadFactory, PointLoadFactory
+from .loads import (
+    DistributedLoadFactory,
+    LoadFactory,
+    MomentLoadFactory,
+    PointLoadFactory,
+)
 from .mesh import MeshFactory
 from .reactions import ReactionFactory
