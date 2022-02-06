24e386e53db369adafd72250d69fdc8f5b665303
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-02-05 21:57:15 -0600

    WIP: Add import for ConstantDistributedLoadFactory

--- tests/factories/loads/__init__.py
+++ tests/factories/loads/__init__.py
@@ -1,6 +1,7 @@
 # pylint: disable=F
 # flake8: noqa
 
+from .constant_distributed import ConstantDistributedLoadFactory
 from .distributed_load import DistributedLoadFactory
 from .load import LoadFactory
 from .moment import MomentLoadFactory
