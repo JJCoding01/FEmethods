e668d62f6460d6de27a9d83000ef699aa6d4700d
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2022-06-28 20:17:07 -0500

    Skip constant distributed load test
    Premature testing of equivalent loads.

--- tests/unit/loads/test_constant_distributed_load.py
+++ tests/unit/loads/test_constant_distributed_load.py
@@ -3,6 +3,7 @@ import pytest
 from tests.factories import ConstantDistributedLoadFactory
 
 
+@pytest.mark.skip(reason="equivalent_loads function not functional yet")
 def test_const_distributed_load_loads():
     dload = ConstantDistributedLoadFactory(start=0, stop=20)
     nodes = dload.equivalent_loads(nodes=(0, 10, 20))
