f29823d3f7f490449d697aee560dc51d6497bdf5
Joseph Contreras <jcontreras@marinetravelift.com>
2025-12-20 10:16:32 -0600

    Rewrite simply supported equal non-symmetric tests

--- tests/integration/simply-supported/point-loads/test_simply_supported_eq_non_symmetric.py
+++ tests/integration/simply-supported/point-loads/test_simply_supported_eq_non_symmetric.py
@@ -1,7 +1,7 @@
 """
 Functional tests for simply supported beams (beams with two pinned reactions)
 
-https://www.awc.org/pdf/codes-standards/publications/design-aids/AWC-DA6-BeamFormulas-0710.pdf
+https://www.structx.com/Beam_Formulas_010.html
 """
 
 import pytest
@@ -9,28 +9,34 @@ import pytest
 from femethods.elements import Beam
 from femethods.loads import PointLoad
 from femethods.reactions import PinnedReaction
-
-from .settings import E, Ixx, TOL
+from tests.factories import MeshFactory
 
 
 @pytest.fixture()
-def beam_setup(beam_length, load):
+def beam_setup(beam_length, load, E, I):
     a = beam_length / 4  # location of first load from right
     b = beam_length / 5  # location of second load from left
 
     p = [PointLoad(magnitude=load, location=x) for x in [a, beam_length - b]]
     r = [PinnedReaction(x) for x in [0, beam_length]]
-    beam = Beam(length=beam_length, loads=p, reactions=r, E=E, Ixx=Ixx)
+    mesh = MeshFactory(
+        length=beam_length,
+        locations=[0, beam_length],
+        node_dof=2,
+        max_element_length=None,
+        min_element_count=None,
+    )
+    beam = Beam(length=beam_length, loads=p, reactions=r, mesh=mesh, E=E, Ixx=I)
     yield beam, beam_length, load, a, b
 
 
 # noinspection PyPep8Naming
 @pytest.mark.parametrize("reaction_index", (0, 1))
-def test_simply_supported_eq_non_symmetric_reaction_force(reaction_index, beam_setup):
-    beam, beam_length, load, a, b = beam_setup
+def test_simply_supported_eq_non_symmetric_reaction_force(reaction_index, beam_setup, TOL):
+    beam, beam_length_, load_, a, b = beam_setup
 
-    R1 = -load / beam_length * (beam_length - a + b)
-    R2 = -load / beam_length * (beam_length + a - b)
+    R1 = -load_ / beam_length_ * (beam_length_ - a + b)
+    R2 = -load_ / beam_length_ * (beam_length_ + a - b)
     R = [R1, R2]
     assert (
         pytest.approx(beam.reactions[reaction_index].force, rel=TOL)
@@ -48,19 +54,19 @@ def test_simply_supported_eq_non_symmetric_reaction_moment_0(
 
 # noinspection PyPep8Naming
 @pytest.mark.parametrize("location", ("load1", "center", "load2"))
-def test_simply_supported_eq_non_symmetric_moment(location, beam_setup):
-    beam, beam_length, load, a, b = beam_setup
+def test_simply_supported_eq_non_symmetric_moment(location, beam_setup, TOL):
+    beam, beam_length_, load_, a, b = beam_setup
 
-    R1 = -load / beam_length * (beam_length - a + b)
-    R2 = -load / beam_length * (beam_length + a - b)
+    R1 = -load_ / beam_length_ * (beam_length_ - a + b)
+    R2 = -load_ / beam_length_ * (beam_length_ + a - b)
 
     M1 = R1 * a  # moment at first load
-    x = beam_length / 2
-    Mx = R1 * x + load * (x - a)  # moment at center
+    x = beam_length_ / 2
+    Mx = R1 * x + load_ * (x - a)  # moment at center
     M2 = R2 * b  # moment at second load
     if location == "load1":
         assert pytest.approx(beam.moment(a), rel=TOL) == M1
     if location == "center":
         assert pytest.approx(beam.moment(x), rel=TOL) == Mx
     if location == "load2":
-        assert pytest.approx(beam.moment(beam_length - b), rel=TOL) == M2
+        assert pytest.approx(beam.moment(beam_length_ - b), rel=TOL) == M2
