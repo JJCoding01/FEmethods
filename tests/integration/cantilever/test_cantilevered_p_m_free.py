9eb6f87bd81f3f3fdf16f2387b599cc43418c2e3
Joseph Contreras <jcontreras@marinetravelift.com>
2025-12-20 11:23:16 -0600

    Add integration tests for cantilevered beam with point and moment at end

---
+++ tests/integration/cantilever/test_cantilevered_p_m_free.py
@@ -0,0 +1,66 @@
+"""
+Functional tests for beams with fixed supports
+
+https://www.structx.com/Beam_Formulas_022.html
+
+"""
+
+import pytest
+
+from femethods.elements import Beam
+from femethods.loads import MomentLoad, PointLoad
+from femethods.reactions import FixedReaction
+from tests.factories import MeshFactory
+
+
+@pytest.fixture()
+def beam_setup(beam_length, load, E, I):
+    """Cantilever beam with load and moment at free end"""
+
+    mesh = MeshFactory(
+        length=beam_length,
+        locations=[0, beam_length],
+        node_dof=2,
+        max_element_length=None,
+        min_element_count=None,
+    )
+
+    beam = Beam(
+        length=beam_length,
+        loads=[
+            PointLoad(magnitude=load, location=0),
+            MomentLoad(magnitude=load, location=0),
+        ],
+        reactions=[FixedReaction(beam_length)],
+        mesh=mesh,
+        E=E,
+        Ixx=I,
+    )
+    yield beam, beam_length, load
+
+
+def test_cantilevered_beam_reaction(beam_setup, TOL):
+    beam, beam_length, load_ = beam_setup
+    assert pytest.approx(beam.reactions[0].force, rel=TOL) == -load_
+    assert pytest.approx(beam.reactions[0].moment, rel=TOL) == load_
+
+
+def test_cantilevered_beam_max_moment(beam_setup, TOL):
+    """
+    fixed beam with concentrated load at free end
+    """
+    beam, L, P = beam_setup
+    # noinspection PyPep8Naming
+    M_max = P * L / 2  # both ends
+    assert pytest.approx(beam.moment(L), rel=TOL) == M_max
+    assert pytest.approx(beam.moment(0), rel=TOL) == M_max
+
+
+def test_cantilevered_beam_load_max_deflection(beam_setup, EI, TOL):
+    """
+    fixed beam with concentrated load at free end
+    """
+    beam, L, P = beam_setup
+
+    d_max = P * L**3 / (12 * EI)  # at free end
+    assert pytest.approx(beam.deflection(0), rel=TOL) == d_max
