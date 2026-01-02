dcf0fec482969edbe203a9600c0a150cadd2563e
Joseph Contreras <jcontreras@marinetravelift.com>
2026-01-02 13:52:37 -0600

    Delete settings.py file

--- tests/integration/settings.py
+++
@@ -1,9 +0,0 @@
-"""
-Common constants and parameters to be used functional tests. This is done so that
-the beams are all similar in size and loading.
-"""
-
-E = 29e6  # psi, Young's modulus
-Ixx = 350  # in^4 area moment of inertia of beam
-EI = E * Ixx  # common constant
-TOL = 1e-1  # allowable tolerance between exact and numerical solutions to pass
